fc6cf03224199f2ad9419e0bde8abf9eef4e28cf
fathur rahman <cyberflyx@gmail.com>
2016-05-08 16:03:50 +0800

    first commit

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+class MY_Controller extends CI_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+  }
+}
+
+class Admin_Controller extends MY_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+  }
+}
+
+class Public_Controller extends MY_Controller
+{
+  function __construct()
+  {
+    parent::__construct();
+  }
+}
